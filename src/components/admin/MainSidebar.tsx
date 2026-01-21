799799747afe6b7c883c7b5b8c9ddf89b18ee08d
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2026-01-21 14:43:56 +0200

    Update MainSidebar.tsx

--- src/components/admin/MainSidebar.tsx
+++ src/components/admin/MainSidebar.tsx
@@ -153,7 +153,7 @@ export function MainSidebar({
               )}
             >
               <Briefcase className="h-5 w-5" />
-              <span>Jobs</span>
+              <span>Requests</span>
             </Button>
           )}
 
